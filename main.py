0a7781411cdbb85edaaff9ae7c529fc1407bb5e9
CodingThings <david.roszkowski@gmail.com>
2025-07-23 23:34:27 -0500

    Added sorting to chars and pretty output

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import check_num_words
 from stats import char_count
+from stats import sort_dict
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -7,10 +8,21 @@ def get_book_text(file_path):
     return file_contents
 
 def main():
-    file_contents = get_book_text("books/frankenstein.txt")
+    file_path = "books/frankenstein.txt"
+    file_contents = get_book_text(file_path)
     num_words = check_num_words(file_contents)
-    print(f"{num_words} words found in the document")
     count = char_count(file_contents)
-    print(count)
+    sorted_count = sort_dict(count)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for dict in sorted_count:
+        if dict["char"].isalpha():
+            print(f"{dict['char']}: {dict['num']}")
+    print("============= END ===============")
+    
 
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,16 @@ def char_count(file_contents):
         else:
             count[char] = 1
     return count
+
+def sort_on(items):
+    return items["num"]
+
+def sort_dict(count):
+    char_list = []
+    for char in count:
+        temp_dict = {}
+        temp_dict["char"] = char
+        temp_dict["num"] = count[char]
+        char_list.append(temp_dict)
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
